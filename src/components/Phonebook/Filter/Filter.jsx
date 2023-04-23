e91ec4d9b24f5124f01762147da01c1089fe6d1a
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-04-23 13:19:01 +0200

    23.04

---
+++ src/components/Phonebook/Filter/Filter.jsx
@@ -0,0 +1,24 @@
+import css from '../ContactForm/Style.module.css';
+import PropTypes from 'prop-types';
+
+const Filter = ({ handleFilterUpdate }) => {
+  return (
+    <div className={css.filter}>
+      <label htmlFor="filter-input">Find contacts by name</label>
+      <input
+        id="filter-input"
+        type="text"
+        name="name"
+        pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
+        title="Filter"
+        onChange={handleFilterUpdate}
+      />
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  handleFilterUpdate: PropTypes.func,
+};
+
+export default Filter;
