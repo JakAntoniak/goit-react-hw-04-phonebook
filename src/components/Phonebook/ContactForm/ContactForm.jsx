df13503e30c700a02cd1a89a68f5845c66cae413
Jakub Antoniak <antonipomorzanin@gmail.com>
2023-07-20 19:46:18 +0200

    Update ContactForm.jsx

--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 
 const ContactForm = ({ addContact }) => {
   return (
-    <form className={css['contact-form']}>
+    <form className={css['contact-form']} onSubmit={addContact}>
       <label htmlFor="name">Name</label>
       <input
         id="name"
         type="text"
         name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        pattern="^[A-Za-z.'\- ]+$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
@@ -18,13 +18,11 @@ const ContactForm = ({ addContact }) => {
         id="number"
         type="tel"
         name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
-      <button className={css.button} type="submit" onClick={addContact}>
-        Add contact
-      </button>
+      <button className={css.button}>Add contact</button>
     </form>
   );
 };
